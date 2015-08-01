4d5669ccb8e7660fb922fcb0fca611ac69c3a4d1
mateo1235 <mateo1235@gmail.com>
2015-08-01 02:43:24 -0500

    WS RESTFul que provee datos de clientes.

---
+++ build/generated-sources/ap-source-output/com/udea/entity/Customer_.java
@@ -0,0 +1,22 @@
+package com.udea.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-08-01T02:21:31")
+@StaticMetamodel(Customer.class)
+public class Customer_ { 
+
+    public static volatile SingularAttribute<Customer, String> city;
+    public static volatile SingularAttribute<Customer, String> phone;
+    public static volatile SingularAttribute<Customer, Integer> customerId;
+    public static volatile SingularAttribute<Customer, String> name;
+    public static volatile SingularAttribute<Customer, String> addressline2;
+    public static volatile SingularAttribute<Customer, Integer> creditLimit;
+    public static volatile SingularAttribute<Customer, String> addressline1;
+    public static volatile SingularAttribute<Customer, String> state;
+    public static volatile SingularAttribute<Customer, String> fax;
+    public static volatile SingularAttribute<Customer, String> email;
+
+}
